5e25ef0d2b0362c5710977a99c8f168dca308e53
Ronaldinho37 <ronaldsilvasantos02@gmail.com>
2024-08-21 11:50:45 -0300

    Delete tcc directory

--- tcc/Aplicativo/static/js/mouse_funcoes.js
+++
@@ -1,8 +0,0 @@
-function entrou(x){
-    let div = document.querySelector(`div[id="${x.dataset.id}"]`)
-    div.style.display = 'flex'
-}
-function saiu(x){
-    let div = document.querySelector(`div[id="${x.dataset.id}"]`)
-    div.style.display = 'none'
-}
