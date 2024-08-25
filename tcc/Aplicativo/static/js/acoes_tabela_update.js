c78a7834035c7951a82bc866acc6b44a74634983
Ronaldinho37 <ronaldsilvasantos02@gmail.com>
2024-08-25 16:40:10 -0300

    Delete tcc directory

--- tcc/Aplicativo/static/js/acoes_tabela_update.js
+++
@@ -1,5 +0,0 @@
-let tabela = document.querySelector('table[id="tabela"]').dataset.user
-
-function adicionar_linha(x){
-    window.location.href = `/update/${tabela}/${x.id}`
-}
--- tcc/Aplicativo/static/js/mouse_funcoes.js
+++
@@ -1,9 +0,0 @@
-function entrou(x){
-    let div = document.querySelector(`div[id="${x.dataset.id}"]`)
-    console.log(div)
-    div.style.display = 'flex'
-}
-function saiu(x){
-    let div = document.querySelector(`div[id="${x.dataset.id}"]`)
-    div.style.display = 'none'
-}
