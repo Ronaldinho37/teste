e94f7fb11694742d7f2541d4d9fedf4f359f4e73
roni <roni>
2024-09-16 09:00:55 -0300

    fff

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bi0#yhc)()x(_x^2c)!s!i!@iehh$v-ga8t&8i23=e0%kiypsn'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get({'DJANGO_DEBUG'}, default=0))
