63c2b02c1ba17cf15ddd2f3e6e75cd62eb47fd49
roni <roni>
2024-09-16 09:55:40 -0300

    f2

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-bi0#yhc)()x(_x^2c)!s!i!@iehh$v-ga8t&8i23=e0%kiypsn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get({'DJANGO_DEBUG'}, default=0))
+DEBUG = 1
 
-ALLOWED_HOSTS = str(os.environ.get({'DJANGO_ALLOWED_HOSTS'})).split(',')
+ALLOWED_HOSTS = ['localhost','127.0.0.1','Roni.pythonanywhere.com']
 
 
 # Application definition
