b4629492798389a5f83c6652a65f29b6d806276d
roni <roni>
2024-09-30 08:36:35 -0300

    novo login

---
+++ Aplicativo/static/js/R_login.js
@@ -0,0 +1,33 @@
+ScrollReveal().reveal('.form-container', {
+    duration: 1000,
+    origin: 'top',
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true
+});
+
+ScrollReveal().reveal('.logo-header', {
+    duration: 1500,
+    origin: 'top',
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true
+});
+
+ScrollReveal().reveal('.espirit', {
+    duration: 1500,
+    origin: 'right',
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true
+});
+
+ScrollReveal().reveal('.typewriter-text', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true
+});
+
+
