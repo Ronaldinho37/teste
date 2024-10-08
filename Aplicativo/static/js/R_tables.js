5cabde31d27d1b142b47713b3e8452a9848737ca
Alef <alefcgoncalves@outlook.com>
2024-10-08 08:33:31 -0300

    mexendo nos updates

---
+++ Aplicativo/static/js/R_tables.js
@@ -0,0 +1,65 @@
+ScrollReveal().reveal('#div-alert-tables', {
+    duration: 1000,
+    origin: 'top',
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true
+});
+
+ScrollReveal().reveal('#search-for', {
+    duration: 1200,
+    origin: 'top',
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true
+});
+
+ScrollReveal().reveal('#in-the-column', {
+    duration: 1200,
+    origin: 'top',
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true
+});
+
+ScrollReveal().reveal('#ordem', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true
+});
+
+ScrollReveal().reveal('#coluna', {
+    duration: 1300,
+    origin: 'bottom',
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true
+});
+
+ScrollReveal().reveal('#selects', {
+    duration: 1300,
+    origin: 'bottom',
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true
+});
+
+ScrollReveal().reveal('#btn-del', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true
+});
+
+ScrollReveal().reveal('#tabela', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true
+});
+
+
